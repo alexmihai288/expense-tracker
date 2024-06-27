44defb41e5b2a429aba5bc299e22fdeb16d065c2
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-27 22:18:19 +0300

    chore - expense app done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { AddTransaction } from "@/components/AddTransaction";
 import { Balance } from "@/components/Balance";
 import { IncomeExpense } from "@/components/IncomeExpense";
-import { TransactionList } from "@/components/TransactionList";
+import TransactionList from "@/components/TransactionList";
 
 const HomePage = async () => {
   const user = await currentUser();
--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,16 +1,25 @@
-import { Transcation } from "@prisma/client";
+import getTransactions from "@/app/actions/getTransactions";
+import TransactionItem from "./TransactionItem";
 import { Transaction } from "@/types/transaction";
 
-export const TransactionList = () => {
+const TransactionList = async () => {
+  const { transactions, error } = await getTransactions();
+
+  if (error) {
+    return <p className="error">{error}</p>;
+  }
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
         {transactions &&
-          TransactionList.map((transaction: Transcation) => (
-            <p>{transaction.text}</p>
+          transactions.map((transaction: Transaction) => (
+            <TransactionItem key={transaction.id} transaction={transaction} />
           ))}
       </ul>
     </>
   );
 };
+
+export default TransactionList;
