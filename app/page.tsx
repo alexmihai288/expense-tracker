431b49c2610eff02a0fdac4dfa749b6a8ab129b0
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-27 13:27:03 +0300

    chore - toast

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import { Guest } from "@/components/Guest"
-import { currentUser } from "@clerk/nextjs/server"
+import { Guest } from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+import { AddTransaction } from "@/components/AddTransaction";
 
 const HomePage = async () => {
-  const user = await currentUser()
+  const user = await currentUser();
 
-  if(!user)
-    return <Guest/>
+  if (!user) return <Guest />;
 
   return (
     <main>
       <h1>Welcome, {user.firstName}</h1>
-      
+      <AddTransaction />
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
